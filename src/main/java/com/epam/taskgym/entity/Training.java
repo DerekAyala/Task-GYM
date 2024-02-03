fbcc4b50483d784854387c0dd3b7d634047c2960
Derek Ayala <derek_ayala@epam.com>
2024-02-03 14:55:21 -0600

    Add Getters, Setters and ToString with lombok

--- src/main/java/com/epam/taskgym/entity/Training.java
+++ src/main/java/com/epam/taskgym/entity/Training.java
@@ -1,6 +1,12 @@
 package com.epam.taskgym.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@ToString
 public class Training {
 
     private Long id;
--- src/main/java/com/epam/taskgym/entity/TrainingType.java
+++ src/main/java/com/epam/taskgym/entity/TrainingType.java
@@ -1,5 +1,12 @@
 package com.epam.taskgym.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class TrainingType {
 
     private Long id;
