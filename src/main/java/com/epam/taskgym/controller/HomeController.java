54842a567612d0826ba4049c7259f4698de9c013
Derek Ayala <derek_ayala@epam.com>
2024-02-05 22:41:38 -0600

    Welcome view

---
+++ src/main/java/com/epam/taskgym/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.epam.taskgym.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "welcome";
+    }
+}
