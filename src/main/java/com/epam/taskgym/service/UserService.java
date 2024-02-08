27d26d16cbf1056dc7274a214477bbb4736f0ce0
Derek Ayala <derek_ayala@epam.com>
2024-02-08 16:56:56 -0600

    UserService methods
    Optional<User> findByUsername(String username)
    Optional<User> findByUsernameAndPassword(String username, String password)
    String generateRandomPassword()
    String generateUniqueUsername(String firstName, String lastName)
    User createUser(Map<String, String> userDetails)

---
+++ src/main/java/com/epam/taskgym/service/UserService.java
@@ -0,0 +1,73 @@
+package com.epam.taskgym.service;
+
+import com.epam.taskgym.entity.User;
+import com.epam.taskgym.repository.UserRepository;
+import com.epam.taskgym.service.exception.MissingAttributes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+
+@Repository
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> findByUsernameAndPassword(String username, String password) {
+        return userRepository.findByUsernameAndPassword(username, password);
+    }
+
+    public String generateRandomPassword() {
+        return new Random().ints(48, 122)
+                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
+                .mapToObj(i -> (char) i)
+                .limit(10)
+                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                .toString();
+    }
+
+    public String generateUniqueUsername(String firstName, String lastName) {
+        String baseUsername = firstName + "." + lastName;
+        String username = baseUsername;
+
+        int suffix = 1;
+
+        while (findByUsername(username).isPresent()) {
+            username = baseUsername + suffix;
+            suffix++;
+        }
+
+        return username;
+    }
+
+    public User createUser(Map<String, String> userDetails) {
+        if (userDetails == null || userDetails.isEmpty()) {
+            throw new MissingAttributes("User details cannot be null or empty");
+        }
+
+        if ((!userDetails.containsKey("firstName") || userDetails.get("firstName").isEmpty()) ||
+                (!userDetails.containsKey("lastName") || userDetails.get("lastName").isEmpty())) {
+            throw new MissingAttributes("First name and lastName are required");
+        }
+        String firstName = userDetails.get("firstName");
+        String lastName = userDetails.get("lastName");
+        String username = generateUniqueUsername(firstName.toLowerCase(), lastName.toLowerCase());
+        String password = generateRandomPassword();
+
+        User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setUsername(username);
+        user.setPassword(password);
+        user = userRepository.save(user);
+
+        return user;
+    }
+}
