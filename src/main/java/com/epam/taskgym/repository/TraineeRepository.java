768a7c8227f06ca14e1edf2f5c2acc3a3c2f6639
Derek Ayala <derek_ayala@epam.com>
2024-02-08 17:51:46 -0600

    find Trainees and Trainers by Username and Username & Password

--- src/main/java/com/epam/taskgym/repository/TraineeRepository.java
+++ src/main/java/com/epam/taskgym/repository/TraineeRepository.java
@@ -3,5 +3,9 @@ package com.epam.taskgym.repository;
 import com.epam.taskgym.entity.Trainee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TraineeRepository extends JpaRepository<Trainee, Long> {
+    Optional<Trainee> findByUserUsername(String username);
+    Optional<Trainee> findByUserUsernameAndUserPassword(String username, String password);
 }
--- src/main/java/com/epam/taskgym/repository/TrainerRepository.java
+++ src/main/java/com/epam/taskgym/repository/TrainerRepository.java
@@ -3,5 +3,9 @@ package com.epam.taskgym.repository;
 import com.epam.taskgym.entity.Trainer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TrainerRepository extends JpaRepository<Trainer, Long> {
+    Optional<Trainer> findByUserUsername(String username);
+    Optional<Trainer> findByUserUsernameAndUserPassword(String username, String password);
 }
