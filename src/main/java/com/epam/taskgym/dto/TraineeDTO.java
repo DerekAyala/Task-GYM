96d5cd2149aa992359a2bb1b51d25faf91a0f43a
Derek Ayala <derek_ayala@epam.com>
2024-02-08 16:49:49 -0600

    update my DTOs

--- src/main/java/com/epam/taskgym/dto/TraineeDTO.java
+++ src/main/java/com/epam/taskgym/dto/TraineeDTO.java
@@ -1,17 +1,20 @@
 package com.epam.taskgym.dto;
 
+import com.epam.taskgym.entity.Trainee;
+import com.epam.taskgym.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TraineeDTO {
-    private Long userId;
+    private User user;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
-    private Long traineeId;
+
+    private Trainee trainee;
     private String dateOfBirth;
     private String address;
 }
--- src/main/java/com/epam/taskgym/dto/TrainerDTO.java
+++ src/main/java/com/epam/taskgym/dto/TrainerDTO.java
@@ -1,17 +1,20 @@
 package com.epam.taskgym.dto;
 
+import com.epam.taskgym.entity.Trainer;
+import com.epam.taskgym.entity.TrainingType;
+import com.epam.taskgym.entity.User;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class TrainerDTO {
-    private Long userId;
+    private User user;
     private String username;
     private String password;
     private String firstName;
     private String lastName;
 
-    private Long trainerId;
-    private String specialization;
+    private Trainer trainer;
+    private TrainingType specialization;
 }
