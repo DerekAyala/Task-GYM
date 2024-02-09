4b839c5f2f1660e410b4e1c6743ded3fdcaeac98
Derek Ayala <derek_ayala@epam.com>
2024-02-08 18:42:04 -0600

    on Trainee Service add functions
    authenticateTrainer(String username, String password)
    Optional<Trainer> getTrainerByUsername(String username)
    Optional<Trainer> getTrainerById(Long id)
    TrainerDTO registerTrainer(Map<String, String> trainerDetails)
    boolean updatePasssword(String username, String password, String newPassword)

--- src/main/java/com/epam/taskgym/service/TrainerService.java
+++ src/main/java/com/epam/taskgym/service/TrainerService.java
@@ -1,100 +1,93 @@
-package com.epam.taskgym.service;
+    package com.epam.taskgym.service;
 
-import com.epam.taskgym.dto.TraineeDTO;
-import com.epam.taskgym.dto.TrainerDTO;
-import com.epam.taskgym.entity.Trainer;
-import com.epam.taskgym.entity.TrainingType;
-import com.epam.taskgym.entity.User;
-import com.epam.taskgym.repository.TrainerRepository;
-import com.epam.taskgym.repository.UserRepository;
-import com.epam.taskgym.service.exception.MissingAttributes;
-import com.epam.taskgym.service.exception.NotFoundException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+    import com.epam.taskgym.dto.TraineeDTO;
+    import com.epam.taskgym.dto.TrainerDTO;
+    import com.epam.taskgym.entity.Trainer;
+    import com.epam.taskgym.entity.TrainingType;
+    import com.epam.taskgym.entity.User;
+    import com.epam.taskgym.repository.TrainerRepository;
+    import com.epam.taskgym.repository.UserRepository;
+    import com.epam.taskgym.service.exception.MissingAttributes;
+    import com.epam.taskgym.service.exception.NotFoundException;
+    import org.springframework.beans.factory.annotation.Autowired;
+    import org.springframework.stereotype.Service;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-import java.util.Optional;
-
-
-@Service
-public class TrainerService {
-
-    @Autowired
-    private TrainerRepository trainerRepository;
-
-    @Autowired
-    private UserService userService;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private TrainingTypeService trainingTypeService;
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(TrainerService.class);
+    import java.util.Map;
+    import java.util.Optional;
 
 
-    public boolean authenticateTrainer(String username, String password) {
-        Optional<Trainer> trainer = trainerRepository.findByUserUsernameAndUserPassword(username, password);
-        return trainer.isPresent();
-    }
+    @Service
+    public class TrainerService {
 
-    public Optional<Trainer> getTrainerByUsername(String username) {
-        return trainerRepository.findByUserUsername(username);
-    }
+        @Autowired
+        private TrainerRepository trainerRepository;
 
-    public Optional<Trainer> getTrainerById(Long id) {
-        return trainerRepository.findById(id);
-    }
+        @Autowired
+        private UserService userService;
 
-    public TrainerDTO registerTrainer(Map<String, String> trainerDetails) {
-        User user = userService.createUser(trainerDetails);
+        @Autowired
+        private UserRepository userRepository;
 
-        Trainer trainer = new Trainer();
-        trainer.setUser(user);
-        if ((!trainerDetails.containsKey("specialization") || trainerDetails.get("specialization").isEmpty())) {
-            throw new MissingAttributes("specialization is required");
+        @Autowired
+        private TrainingTypeService trainingTypeService;
+
+        private static final Logger LOGGER = LoggerFactory.getLogger(TrainerService.class);
+
+
+        public boolean authenticateTrainer(String username, String password) {
+            return trainerRepository.findByUserUsernameAndUserPassword(username, password).isPresent();
         }
 
-        Optional<TrainingType> specialization = trainingTypeService.getTrainingTypeById(Long.parseLong(trainerDetails.get("specialization")));
-
-        if (specialization.isEmpty()) {
-            throw new NotFoundException("Specialization with id {" + Long.parseLong(trainerDetails.get("specialization")) + "}found");
+        public Optional<Trainer> getTrainerByUsername(String username) {
+            return trainerRepository.findByUserUsername(username);
         }
 
-        trainer.setSpecialization(specialization.get());
-        trainer = trainerRepository.save(trainer);
-        LOGGER.info("Trainer saved with ID: {}", trainer.getId());
+        public Optional<Trainer> getTrainerById(Long id) {
+            return trainerRepository.findById(id);
+        }
 
-        return fillTrainerDTO(user, trainer);
-    }
+        public TrainerDTO registerTrainer(Map<String, String> trainerDetails) {
+            User user = userService.createUser(trainerDetails);
 
-    public boolean updatePasssword(String username, String password, String newPassword) {
-        if (authenticateTrainer(username, password)) {
-            Optional<Trainer> trainerOptional = getTrainerByUsername(username);
-            if (trainerOptional.isPresent()) {
-                Trainer trainer = trainerOptional.get();
-                User user = trainer.getUser();
-                user.setPassword(newPassword);
-                userRepository.save(user);
-                trainer.setUser(user);
-                trainerRepository.save(trainer);
-                return true;
+            Trainer trainer = new Trainer();
+            trainer.setUser(user);
+            if ((!trainerDetails.containsKey("specialization") || trainerDetails.get("specialization").isEmpty())) {
+                throw new MissingAttributes("specialization is required");
             }
+
+            Optional<TrainingType> specialization = trainingTypeService.getTrainingTypeById(Long.parseLong(trainerDetails.get("specialization")));
+
+            if (specialization.isEmpty()) {
+                throw new NotFoundException("Specialization with id {" + Long.parseLong(trainerDetails.get("specialization")) + "}found");
+            }
+
+            trainer.setSpecialization(specialization.get());
+            trainer = trainerRepository.save(trainer);
+            LOGGER.info("Trainer saved with ID: {}", trainer.getId());
+
+            return fillTrainerDTO(user, trainer);
         }
-        return false;
-    }
 
-    private TrainerDTO fillTrainerDTO(User user, Trainer trainer) {
-        TrainerDTO trainerDTO = new TrainerDTO();
-        trainerDTO.setUser(user);
-        trainerDTO.setFirstName(user.getFirstName());
-        trainerDTO.setLastName(user.getLastName());
-        trainerDTO.setUsername(user.getUsername());
-        trainerDTO.setSpecialization(trainer.getSpecialization());
-        return trainerDTO;
-    }
+        public boolean updatePasssword(String username, String password, String newPassword) {
+            if (authenticateTrainer(username, password)) {
+                Optional<Trainer> trainerOptional = getTrainerByUsername(username);
+                if (trainerOptional.isPresent()) {
+                    Trainer trainer = trainerOptional.get();
+                    User user = trainer.getUser();
+                    user.setPassword(newPassword);
+                    userRepository.save(user);
+                    trainer.setUser(user);
+                    trainerRepository.save(trainer);
+                    return true;
+                }
+            }
+            return false;
+        }
 
-}
+        private TrainerDTO fillTrainerDTO(User user, Trainer trainer) {
+            return new TrainerDTO(user, user.getUsername(), user.getPassword(), user.getFirstName(), user.getLastName(), trainer, trainer.getSpecialization());
+        }
+
+    }
