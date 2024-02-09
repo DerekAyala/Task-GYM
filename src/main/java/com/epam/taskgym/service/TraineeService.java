9c685c47f704493f79338b0fef06bcb0a84ce86a
Derek Ayala <derek_ayala@epam.com>
2024-02-08 18:04:56 -0600

    Authenticate, find by Username and Id,  update password on Trainee

--- src/main/java/com/epam/taskgym/service/TraineeService.java
+++ src/main/java/com/epam/taskgym/service/TraineeService.java
@@ -1,30 +1,49 @@
 package com.epam.taskgym.service;
 
 import com.epam.taskgym.dto.TraineeDTO;
+import com.epam.taskgym.entity.Trainee;
+import com.epam.taskgym.entity.User;
+import com.epam.taskgym.repository.TraineeRepository;
+import com.epam.taskgym.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
-import java.util.Random;
+import java.util.Optional;
 
 @Service
 public class TraineeService {
 
 
+    @Autowired
+    private TraineeRepository traineeRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(TraineeService.class);
 
 
     public boolean authenticateTrainee(String username, String password) {
-        /*
-        Trainee trainee = traineeDAO.findByUsernameAndPassword(username, password);
-        return trainee != null;
-
-         */
-        return true;
+        Optional<Trainee> trainee = traineeRepository.findByUserUsernameAndUserPassword(username, password);
+        return trainee.isPresent();
     }
 
-    public TraineeDTO registerTrainee(String firstName, String lastName, String dateOfBirth, String address) {
+    public Optional<Trainee> getTraineeByUsername(String username) {
+        return traineeRepository.findByUserUsername(username);
+    }
+
+    public Optional<Trainee> getTraineeById(Long id) {
+        return traineeRepository.findById(id);
+    }
+
+    public TraineeDTO registerTrainee(Map<String, String> traineeDetails) {
+
         /*
         String username = generateUniqueUsername(firstName.toLowerCase(), lastName.toLowerCase());
         String password = generateRandomPassword();
@@ -50,123 +69,19 @@ public class TraineeService {
         return new TraineeDTO();
     }
 
-    public TraineeDTO getTrainee(String username) {
-        /*
-        Trainee trainee = traineeDAO.findByUsername(username);
-        if (trainee != null) {
-            Optional<User> userOptional = userDAO.findById(trainee.getUserId());
-            if (userOptional.isPresent()) {
-                LOGGER.info("Trainee is present");
-                User user = userOptional.get();
-
-                TraineeDTO traineeDTO = new TraineeDTO();
-                fillTraineeDTO(traineeDTO, user, trainee);
-                return traineeDTO;
+    public boolean updatePasssword(String username, String password, String newPassword) {
+        if (authenticateTrainee(username, password)) {
+            Optional<Trainee> traineeOptional = traineeRepository.findByUserUsername(username);
+            if (traineeOptional.isPresent()) {
+                Trainee trainee = traineeOptional.get();
+                User user = trainee.getUser();
+                user.setPassword(newPassword);
+                userRepository.save(user);
+                trainee.setUser(user);
+                traineeRepository.save(trainee);
+                return true;
             }
         }
-        */
-        return null;
-    }
-
-    public TraineeDTO updateTrainee(String username, Map<String, String> updates) {
-        /*
-        User user = userDAO.findByUsername(username)
-                .orElseThrow(() -> new RuntimeException("User not found"));
-        LOGGER.info("User was found by username");
-        Trainee trainee = traineeDAO.findByUserId(user.getId());
-        if (trainee == null) {
-            throw new RuntimeException("Trainee not found");
-        }
-        LOGGER.info("Trainee was found by user id");
-
-        if (updates.containsKey("firstName")) {
-            LOGGER.info("Trainee updates contains firstName");
-            String firstName = updates.get("firstName");
-            user.setFirstName(firstName);
-        }
-
-        if (updates.containsKey("lastName")) {
-            LOGGER.info("Trainee updates contains lastName");
-            String lastName = updates.get("lastName");
-            user.setLastName(lastName);
-        }
-
-        userDAO.update(user);
-
-        if (updates.containsKey("dateOfBirth")) {
-            LOGGER.info("Trainee updates contains dateOfBirth");
-            String dob = updates.get("dateOfBirth");
-            trainee.setDateOfBirth(dob);
-        }
-
-        if (updates.containsKey("address")) {
-            LOGGER.info("Trainee updates contains address");
-            String addr = updates.get("address");
-            trainee.setAddress(addr);
-        }
-
-        traineeDAO.update(trainee);
-
-        TraineeDTO traineeDTO = new TraineeDTO();
-        fillTraineeDTO(traineeDTO, user, trainee);
-        */
-        return new TraineeDTO();
-    }
-
-    public void deleteTrainee(String username) {
-        /*
-        Optional<User> user = userDAO.findByUsername(username);
-        if (user.isPresent()) {
-            LOGGER.info("User was found by username");
-            Trainee trainee = traineeDAO.findByUserId(user.get().getId());
-            if (trainee != null) {
-                traineeDAO.deleteById(trainee.getId());
-                userDAO.deleteById(user.get().getId());
-                LOGGER.info("User and Trainee were deleted");
-            } else {
-                throw new RuntimeException("Trainee not found");
-            }
-        } else {
-            throw new RuntimeException("User not found");
-        }
-        */
-    }
-    /*
-
-    private void fillTraineeDTO(TraineeDTO traineeDTO, User user, Trainee trainee) {
-        traineeDTO.setUserId(user.getId());
-        traineeDTO.setUsername(user.getUsername());
-        traineeDTO.setPassword(user.getPassword());
-        traineeDTO.setFirstName(user.getFirstName());
-        traineeDTO.setLastName(user.getLastName());
-        traineeDTO.setTraineeId(trainee.getId());
-        traineeDTO.setDateOfBirth(trainee.getDateOfBirth());
-        traineeDTO.setAddress(trainee.getAddress());
-    }
-    */
-
-    public String generateUniqueUsername(String firstName, String lastName) {
-
-        String baseUsername = firstName + "." + lastName;
-        String username = baseUsername;
-        /*
-        int suffix = 1;
-
-        while (userDAO.findByUsername(username).isPresent()) {
-            username = baseUsername + suffix;
-            suffix++;
-        }
-         */
-
-        return username;
-    }
-
-    public String generateRandomPassword() {
-        return new Random().ints(48, 122)
-                .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
-                .mapToObj(i -> (char) i)
-                .limit(10)
-                .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
-                .toString();
+        return false;
     }
 }
