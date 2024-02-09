8fdb0872ff1630be0cb0b99139784a39b982e7df
Derek Ayala <derek_ayala@epam.com>
2024-02-08 18:38:01 -0600

    add allArgsContructor and ToString to DTOs

--- src/main/java/com/epam/taskgym/dto/TraineeDTO.java
+++ src/main/java/com/epam/taskgym/dto/TraineeDTO.java
@@ -2,11 +2,15 @@ package com.epam.taskgym.dto;
 
 import com.epam.taskgym.entity.Trainee;
 import com.epam.taskgym.entity.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@ToString
 public class TraineeDTO {
     private User user;
     private String username;
