a8ec02dd5e36f3fc74fe8b1d281eff9ffe33d7d9
Derek Ayala <derek_ayala@epam.com>
2024-02-12 16:11:25 -0600

    add query functions to TrainingRepository

--- src/main/java/com/epam/taskgym/repository/TrainingRepository.java
+++ src/main/java/com/epam/taskgym/repository/TrainingRepository.java
@@ -1,7 +1,17 @@
 package com.epam.taskgym.repository;
 
+import com.epam.taskgym.entity.Trainee;
 import com.epam.taskgym.entity.Training;
+import lombok.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface TrainingRepository extends JpaRepository<Training, Long> {
+    List<Training> findAllByTrainee_User_Username(String username);
+    List<Training> findAllByTrainee_User_UsernameOrderByTrainer_User_firstName(String username);
+    List<Training> findAllByTrainee_User_UsernameAndTrainingType_name(@NonNull String username, @NonNull String trainingType);
+    List<Training> findAllByTrainer_User_Username(String username);
+    List<Training> findAllByTrainer_User_UsernameOrderByTrainee_User_firstNameAsc(String username);
 }
