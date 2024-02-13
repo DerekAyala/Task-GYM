fe8bbb08e0fb87c9777ff4731532eed63f01fad6
Derek Ayala <derek_ayala@epam.com>
2024-02-12 19:40:42 -0600

    new exceptions

---
+++ src/main/java/com/epam/taskgym/service/exception/InvalidPasswordException.java
@@ -0,0 +1,7 @@
+package com.epam.taskgym.service.exception;
+
+public class InvalidPasswordException extends RuntimeException {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
