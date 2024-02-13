26ccca2e96af1231a144710e7189f9c353b00329
Derek Ayala <derek_ayala@epam.com>
2024-02-12 18:01:48 -0600

    Create Training and all findBy with username and criteria asked

--- src/main/java/com/epam/taskgym/service/TrainingService.java
+++ src/main/java/com/epam/taskgym/service/TrainingService.java
@@ -4,6 +4,8 @@ import com.epam.taskgym.entity.Trainee;
 import com.epam.taskgym.entity.Trainer;
 import com.epam.taskgym.entity.Training;
 import com.epam.taskgym.entity.TrainingType;
+import com.epam.taskgym.repository.TrainingRepository;
+import com.epam.taskgym.service.exception.BadRequestException;
 import com.epam.taskgym.service.exception.MissingAttributes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -33,27 +36,68 @@ public class TrainingService {
         }
 
         if ((!trainingDetails.containsKey("traineeUsername") || trainingDetails.get("traineeUsername").isEmpty()) &&
-            (!trainingDetails.containsKey("trainerUsername") || trainingDetails.get("trainerUsername").isEmpty()) &&
-            (!trainingDetails.containsKey("trainingTypeName") || trainingDetails.get("trainingTypeName").isEmpty()) &&
-            (!trainingDetails.containsKey("date") || trainingDetails.get("date").isEmpty()) &&
-            (!trainingDetails.containsKey("duration") || trainingDetails.get("duration").isEmpty()) &&
-            (!trainingDetails.containsKey("name") || trainingDetails.get("name").isEmpty())){
+                (!trainingDetails.containsKey("trainerUsername") || trainingDetails.get("trainerUsername").isEmpty()) &&
+                (!trainingDetails.containsKey("trainingTypeName") || trainingDetails.get("trainingTypeName").isEmpty()) &&
+                (!trainingDetails.containsKey("date") || trainingDetails.get("date").isEmpty()) &&
+                (!trainingDetails.containsKey("duration") || trainingDetails.get("duration").isEmpty()) &&
+                (!trainingDetails.containsKey("name") || trainingDetails.get("name").isEmpty())) {
             throw new MissingAttributes("Trainee, trainer, trainingType, name, date and duration are required");
         }
+
         Trainee trainee = traineeService.getTraineeByUsername(trainingDetails.get("traineeUsername"));
         Trainer trainer = trainerService.getTrainerByUsername(trainingDetails.get("trainerUsername"));
-        TrainingType trainingType = trainingTypeService.getTrainingTypeByName(trainingDetails.get("trainingTypeName"));
         Date date = traineeService.validateDate(trainingDetails.get("date"));
+        TrainingType trainingType = trainingTypeService.getTrainingTypeByName(trainingDetails.get("trainingTypeName"));
+        int duration = validateDuration(trainingDetails.get("duration"));
+
         Training training = new Training();
         training.setTrainee(trainee);
         training.setTrainer(trainer);
-        training.setTrainingType(trainingType);
         training.setName(trainingDetails.get("name"));
+        training.setDate(date);
+        training.setTrainingType(trainingType);
+        training.setDuration(duration);
 
-        return new Training();
+        return training;
     }
 
-    public Training getTraining(Long id) {
-        return new Training();
+    public Integer validateDuration(String duration) {
+        try {
+            return Integer.parseInt(duration);
+        } catch (NumberFormatException e) {
+            throw new BadRequestException("Duration must be a number");
+        }
+    }
+
+    public List<Training> getTrainingsByTraineeUsername(String username) {
+        return trainingRepository.findAllByTrainee_User_Username(username);
+    }
+
+    public void deleteTrainingsByTraineeUsername(String username) {
+        trainingRepository.deleteAllByTrainee_User_Username(username);
+    }
+
+    public List<Training> getTrainingsByTraineeUsernameAndDateBetween(String username, Date startDate, Date endDate) {
+        return trainingRepository.findAllByTrainee_User_UsernameAndDateBetween(username, startDate, endDate);
+    }
+
+    public List<Training> getTrainingsByTraineeUsernameAndTrainerName(String username, String trainerName) {
+        return trainingRepository.findAllByTrainee_User_UsernameAndTrainer_User_FirstName(username, trainerName);
+    }
+
+    public List<Training> getTrainingsByTraineeUsernameAndTrainingTypeName(String username, String trainingType) {
+        return trainingRepository.findAllByTrainee_User_UsernameAndTrainingType_name(username, trainingType);
+    }
+
+    public List<Training> getTrainingsByTrainerUsername(String username) {
+        return trainingRepository.findAllByTrainer_User_Username(username);
+    }
+
+    public List<Training> getTrainingsByTrainerUsernameAndDateBetween(String username, Date startDate, Date endDate) {
+        return trainingRepository.findAllByTrainer_User_UsernameAndDateBetween(username, startDate, endDate);
+    }
+
+    public List<Training> getTrainingsByTrainerUsernameAndTraineeName(String username, String traineeName) {
+        return trainingRepository.findAllByTrainer_User_UsernameAndTrainee_User_FirstName(username, traineeName);
     }
 }
