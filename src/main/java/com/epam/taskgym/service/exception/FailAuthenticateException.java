ae736da4dac3e2f7f1903adc0be5ed49880ec10a
Derek Ayala <derek_ayala@epam.com>
2024-02-12 19:40:13 -0600

    delete unused constructor exceptions

--- src/main/java/com/epam/taskgym/service/exception/FailAuthenticateException.java
+++ src/main/java/com/epam/taskgym/service/exception/FailAuthenticateException.java
@@ -4,15 +4,4 @@ public class FailAuthenticateException extends RuntimeException{
     public FailAuthenticateException(String message) {
         super(message);
     }
-
-    public FailAuthenticateException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public FailAuthenticateException(Throwable cause) {
-        super(cause);
-    }
-
-    public FailAuthenticateException() {
-    }
 }
--- src/main/java/com/epam/taskgym/service/exception/MissingAttributes.java
+++ src/main/java/com/epam/taskgym/service/exception/MissingAttributes.java
@@ -4,15 +4,4 @@ public class MissingAttributes extends RuntimeException {
     public MissingAttributes(String message) {
         super(message);
     }
-
-    public MissingAttributes(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public MissingAttributes(Throwable cause) {
-        super(cause);
-    }
-
-    public MissingAttributes() {
-    }
 }
