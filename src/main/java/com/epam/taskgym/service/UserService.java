1c867d3ea7e416b165339181cd667b1f5b33ae75
Derek Ayala <derek_ayala@epam.com>
2024-02-12 19:05:58 -0600

    improve User Repository with Single Responsibility Principle

--- src/main/java/com/epam/taskgym/service/UserService.java
+++ src/main/java/com/epam/taskgym/service/UserService.java
@@ -4,8 +4,10 @@ import com.epam.taskgym.entity.User;
 import com.epam.taskgym.repository.UserRepository;
 import com.epam.taskgym.service.exception.BadRequestException;
 import com.epam.taskgym.service.exception.MissingAttributes;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -15,17 +17,72 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.Random;
 
-@Repository
+@Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
-    public Optional<User> findByUsername(String username) {
+    private Optional<User> findByUsername(String username) {
         return userRepository.findByUsername(username);
     }
 
-    public String generateRandomPassword() {
+    @Transactional
+    public User createUser(Map<String, String> userDetails) {
+        validateUserDetails(userDetails);
+        String username = generateUniqueUsername(userDetails.get("firstName").toLowerCase(), userDetails.get("lastName").toLowerCase());
+        String password = generateRandomPassword();
+
+        User user = buildUser(userDetails, username, password);
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @Transactional
+    public User updateUser(Map<String, String> userDetails, User user) {
+        user.setFirstName(userDetails.getOrDefault("firstName", user.getFirstName()));
+        user.setLastName(userDetails.getOrDefault("lastName", user.getLastName()));
+        userRepository.save(user);
+
+        return user;
+    }
+
+    @Transactional
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public User toggleUserActivation(User user) {
+        user.setIsActive(!user.getIsActive());
+        userRepository.save(user);
+        return user;
+    }
+
+    @Transactional
+    public void deleteUser(User user) {
+        userRepository.delete(user);
+    }
+
+    private void validateUserDetails(Map<String, String> userDetails) {
+        if ((!userDetails.containsKey("firstName") || userDetails.get("firstName").isEmpty()) ||
+                (!userDetails.containsKey("lastName") || userDetails.get("lastName").isEmpty())) {
+            throw new MissingAttributes("First name and lastName are required");
+        }
+    }
+
+    private User buildUser(Map<String, String> userDetails, String username, String password) {
+        User user = new User();
+        user.setFirstName(userDetails.get("firstName"));
+        user.setLastName(userDetails.get("lastName"));
+        user.setUsername(username);
+        user.setPassword(password);
+        user.setIsActive(true);
+        return user;
+    }
+
+    private String generateRandomPassword() {
         return new Random().ints(48, 122)
                 .filter(i -> (i <= 57 || i >= 65) && (i <= 90 || i >= 97))
                 .mapToObj(i -> (char) i)
@@ -34,7 +91,7 @@ public class UserService {
                 .toString();
     }
 
-    public String generateUniqueUsername(String firstName, String lastName) {
+    private String generateUniqueUsername(String firstName, String lastName) {
         String baseUsername = firstName + "." + lastName;
         String username = baseUsername;
 
@@ -46,48 +103,5 @@ public class UserService {
         }
 
         return username;
-    }
-
-    public User createUser(Map<String, String> userDetails) {
-        if ((!userDetails.containsKey("firstName") || userDetails.get("firstName").isEmpty()) ||
-                (!userDetails.containsKey("lastName") || userDetails.get("lastName").isEmpty())) {
-            throw new MissingAttributes("First name and lastName are required");
-        }
-        String firstName = userDetails.get("firstName");
-        String lastName = userDetails.get("lastName");
-        String username = generateUniqueUsername(firstName.toLowerCase(), lastName.toLowerCase());
-        String password = generateRandomPassword();
-
-        User user = new User();
-        user.setFirstName(firstName);
-        user.setLastName(lastName);
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setIsActive(true);
-        userRepository.save(user);
-
-        return user;
-    }
-
-    public User updateUser(Map<String, String> userDetails, User user) {
-        user.setFirstName(userDetails.getOrDefault("firstName", user.getFirstName()));
-        user.setLastName(userDetails.getOrDefault("lastName", user.getLastName()));
-        userRepository.save(user);
-
-        return user;
-    }
-
-    public User saveUser(User user) {
-        return userRepository.save(user);
-    }
-
-    public User toggleUserActivation(User user) {
-        user.setIsActive(!user.getIsActive());
-        userRepository.save(user);
-        return user;
-    }
-
-    public void deleteUser(User user) {
-        userRepository.delete(user);
     }
 }
