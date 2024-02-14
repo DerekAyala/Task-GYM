2147ea70c7ba14aaebc65f205a8b9da9e09359a7
Derek Ayala <derek_ayala@epam.com>
2024-02-13 19:50:49 -0600

    delete setter annotation

--- src/main/java/com/epam/taskgym/dto/TraineeDTO.java
+++ src/main/java/com/epam/taskgym/dto/TraineeDTO.java
@@ -1,16 +1,12 @@
 package com.epam.taskgym.dto;
 
-import com.epam.taskgym.entity.Trainee;
-import com.epam.taskgym.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Date;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @ToString
 public class TraineeDTO {
--- src/main/java/com/epam/taskgym/dto/TrainerDTO.java
+++ src/main/java/com/epam/taskgym/dto/TrainerDTO.java
@@ -1,15 +1,11 @@
 package com.epam.taskgym.dto;
 
-import com.epam.taskgym.entity.Trainer;
 import com.epam.taskgym.entity.TrainingType;
-import com.epam.taskgym.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.ToString;
 
 @Getter
-@Setter
 @AllArgsConstructor
 @ToString
 public class TrainerDTO {
