6afd80a870cfb4e85c226e3d9f780aec0dd7fa8f
Derek Ayala <derek_ayala@epam.com>
2024-02-19 17:30:28 -0600

    new query on Training Repository

--- src/main/java/com/epam/taskgym/repository/TrainingRepository.java
+++ src/main/java/com/epam/taskgym/repository/TrainingRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TrainingRepository extends JpaRepository<Training, Long> {
@@ -19,6 +20,7 @@ public interface TrainingRepository extends JpaRepository<Training, Long> {
     List<Training> findAllByTrainer_User_Username(String username);
     List<Training> findAllByTrainer_User_UsernameAndDateBetween(String username, Date startDate, Date endDate);
     List<Training> findAllByTrainer_User_UsernameAndTrainee_User_FirstName(String username, String traineeName);
+    Optional<Training> findByTrainee_User_UsernameAndTrainer_User_UsernameAndDateAndTrainingType_Name(String traineeUsername, String trainerUsername, Date date, String trainingTypeName);
     void deleteAllByTrainee_User_Username(String username);
 
     @Query("SELECT DISTINCT t.trainer FROM Training t WHERE t.trainee.user.username = :username")
