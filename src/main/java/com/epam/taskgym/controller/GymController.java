8b4110aee9fa061028ebebf6551ed2841a5e9eeb
Derek Ayala <derek_ayala@epam.com>
2024-02-19 21:32:24 -0600

    Trainer Registration (POST method)

--- src/main/java/com/epam/taskgym/controller/GymController.java
+++ src/main/java/com/epam/taskgym/controller/GymController.java
@@ -2,6 +2,7 @@ package com.epam.taskgym.controller;
 
 import com.epam.taskgym.controller.response.RegisterResponse;
 import com.epam.taskgym.entity.Trainee;
+import com.epam.taskgym.entity.Trainer;
 import com.epam.taskgym.entity.TrainingType;
 import com.epam.taskgym.service.TraineeService;
 import com.epam.taskgym.service.TrainerService;
@@ -33,6 +34,13 @@ public class GymController {
     public ResponseEntity<RegisterResponse> registerTrainee(@RequestBody Map<String, String> traineeDetails) {
         Trainee trainee = traineeService.registerTrainee(traineeDetails);
         return new ResponseEntity<>(new RegisterResponse(trainee.getUser().getUsername(),trainee.getUser().getPassword()), HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/trainer", consumes = "application/json", produces = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<RegisterResponse> registerTrainer(@RequestBody Map<String, String> trainerDetails) {
+        Trainer trainer = trainerService.registerTrainer(trainerDetails);
+        return new ResponseEntity<>(new RegisterResponse(trainer.getUser().getUsername(),trainer.getUser().getPassword()), HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/trainingtypes", method = RequestMethod.GET)
