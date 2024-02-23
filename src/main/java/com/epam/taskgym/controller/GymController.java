8667b41299c4a30bc5ef01189f3138942fe30358
Derek Ayala <derek_ayala@epam.com>
2024-02-23 08:23:43 -0600

    update swagger version

--- src/main/java/com/epam/taskgym/controller/GymController.java
+++ src/main/java/com/epam/taskgym/controller/GymController.java
@@ -6,6 +6,9 @@ import com.epam.taskgym.entity.Trainer;
 import com.epam.taskgym.entity.TrainingType;
 import com.epam.taskgym.entity.User;
 import com.epam.taskgym.service.*;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Api(tags = "Gym Controller", description = "Endpoints for managing gym operations")
 public class GymController {
 
     private final TraineeService traineeService;
@@ -32,33 +36,44 @@ public class GymController {
 
     // 1. Add a new trainee
     @PostMapping(value = "/trainee")
-    public ResponseEntity<RegisterResponseDTO> registerTrainee(@RequestBody TraineeDTO traineeDTO) {
+    @ApiOperation(value = "Add a new trainee", response = ResponseEntity.class)
+    public ResponseEntity<RegisterResponseDTO> registerTrainee(@ApiParam(value = "Trainee details", required = true) @RequestBody TraineeDTO traineeDTO) {
         Trainee trainee = traineeService.registerTrainee(traineeDTO);
-        return new ResponseEntity<>(new RegisterResponseDTO(trainee.getUser().getUsername(),trainee.getUser().getPassword()), HttpStatus.CREATED);
+        return new ResponseEntity<>(new RegisterResponseDTO(trainee.getUser().getUsername(), trainee.getUser().getPassword()), HttpStatus.CREATED);
     }
 
     // 2. Add a new trainer
     @PostMapping(value = "/trainer")
-    public ResponseEntity<RegisterResponseDTO> registerTrainer(@RequestBody TrainerDTO trainerDTO) {
+    @ApiOperation(value = "Add a new trainer", response = ResponseEntity.class)
+    public ResponseEntity<RegisterResponseDTO> registerTrainer(@ApiParam(value = "Trainer details", required = true) @RequestBody TrainerDTO trainerDTO) {
         Trainer trainer = trainerService.registerTrainer(trainerDTO);
-        return new ResponseEntity<>(new RegisterResponseDTO(trainer.getUser().getUsername(),trainer.getUser().getPassword()), HttpStatus.CREATED);
+        return new ResponseEntity<>(new RegisterResponseDTO(trainer.getUser().getUsername(), trainer.getUser().getPassword()), HttpStatus.CREATED);
     }
 
     // 3. Login
     @GetMapping(value = "/user/login")
-    public ResponseEntity<User> loginUser(@RequestParam String username, @RequestParam String password) {
+    @ApiOperation(value = "Login user", response = ResponseEntity.class)
+    public ResponseEntity<User> loginUser(
+            @ApiParam(value = "Username", required = true) @RequestParam String username,
+            @ApiParam(value = "Password", required = true) @RequestParam String password) {
         return new ResponseEntity<>(userService.authenticateUser(username, password), HttpStatus.OK);
     }
 
     // 4. Update Password
     @PutMapping(value = "/user/{username}/password")
-    public ResponseEntity<User> updatePassword(@PathVariable String username, @RequestParam String oldPassword, @RequestParam String newPassword) {
+    @ApiOperation(value = "Update password for a user", response = ResponseEntity.class)
+    public ResponseEntity<User> updatePassword(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Old password", required = true) @RequestParam String oldPassword,
+            @ApiParam(value = "New password", required = true) @RequestParam String newPassword) {
         return new ResponseEntity<>(userService.updatePassword(username, oldPassword, newPassword), HttpStatus.OK);
     }
 
     // 5. Get Trainee Profile by Username
     @GetMapping(value = "/trainees/{username}")
-    public ResponseEntity<TraineeDTO> getTraineeProfile(@PathVariable String username) {
+    @ApiOperation(value = "Get trainee profile by username", response = ResponseEntity.class)
+    public ResponseEntity<TraineeDTO> getTraineeProfile(
+            @ApiParam(value = "Username", required = true) @PathVariable String username) {
         Trainee trainee = traineeService.getTraineeByUsername(username);
         TraineeDTO traineeDTO = traineeService.convertTraineeToTraineeDTO(trainee);
         return new ResponseEntity<>(traineeDTO, HttpStatus.OK);
@@ -66,7 +81,11 @@ public class GymController {
 
     // 6. Update Trainee Profile
     @PutMapping(value = "/trainees/{username}")
-    public ResponseEntity<TraineeDTO> updateTraineeProfile(@PathVariable String username, @RequestParam String password, @RequestBody TraineeDTO traineeDTO) {
+    @ApiOperation(value = "Update trainee profile", response = ResponseEntity.class)
+    public ResponseEntity<TraineeDTO> updateTraineeProfile(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Password", required = true) @RequestParam String password,
+            @ApiParam(value = "Trainee details", required = true) @RequestBody TraineeDTO traineeDTO) {
         Trainee trainee = traineeService.updateTrainee(traineeDTO, username, password);
         TraineeDTO traineeDTOResponse = traineeService.convertTraineeToTraineeDTO(trainee);
         return new ResponseEntity<>(traineeDTOResponse, HttpStatus.OK);
@@ -74,14 +93,19 @@ public class GymController {
 
     // 7. Delete Trainee Profile
     @DeleteMapping(value = "/trainees/{username}")
-    public ResponseEntity<String> deleteTraineeProfile(@PathVariable String username, @RequestParam String password) {
+    @ApiOperation(value = "Delete trainee profile by username", response = ResponseEntity.class)
+    public ResponseEntity<String> deleteTraineeProfile(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Password", required = true) @RequestParam String password) {
         traineeService.deleteTrainee(username, password);
         return new ResponseEntity<>("Trainee profile deleted successfully", HttpStatus.OK);
     }
 
     // 8. Get Trainer Profile by Username
     @GetMapping(value = "/trainers/{username}")
-    public ResponseEntity<TrainerDTO> getTrainerProfile(@PathVariable String username) {
+    @ApiOperation(value = "Get trainer profile by username", response = ResponseEntity.class)
+    public ResponseEntity<TrainerDTO> getTrainerProfile(
+            @ApiParam(value = "Username", required = true) @PathVariable String username) {
         Trainer trainer = trainerService.getTrainerByUsername(username);
         TrainerDTO trainerDTO = trainerService.convertTrainerToTraineeDTO(trainer);
         return new ResponseEntity<>(trainerDTO, HttpStatus.OK);
@@ -89,7 +113,11 @@ public class GymController {
 
     // 9. Update Trainer Profile
     @PutMapping(value = "/trainers/{username}")
-    public ResponseEntity<TrainerDTO> updateTrainerProfile(@PathVariable String username, @RequestParam String password, @RequestBody TrainerDTO trainerDTO) {
+    @ApiOperation(value = "Update trainer profile", response = ResponseEntity.class)
+    public ResponseEntity<TrainerDTO> updateTrainerProfile(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Password", required = true) @RequestParam String password,
+            @ApiParam(value = "Trainer details", required = true) @RequestBody TrainerDTO trainerDTO) {
         Trainer trainer = trainerService.updateTrainer(trainerDTO, username, password);
         TrainerDTO trainerDTOResponse = trainerService.convertTrainerToTraineeDTO(trainer);
         return new ResponseEntity<>(trainerDTOResponse, HttpStatus.OK);
@@ -97,51 +125,74 @@ public class GymController {
 
     // 10. Get not assigned on trainee active trainers
     @GetMapping(value = "/trainees/{username}/trainersNotAssigned")
-    public ResponseEntity<List<Trainer>> getNotAssignedTrainers(@PathVariable String username) {
+    @ApiOperation(value = "Get trainers not assigned to the trainee", response = ResponseEntity.class)
+    public ResponseEntity<List<Trainer>> getNotAssignedTrainers(
+            @ApiParam(value = "Username", required = true) @PathVariable String username) {
         return new ResponseEntity<>(trainerService.getUnassignedTrainers(username), HttpStatus.OK);
     }
 
     // 11. update Trainee's Trainer List
     @PutMapping(value = "/trainees/{username}/trainers")
-    public ResponseEntity<List<TrainerListItem>> updateTraineeTrainers(@PathVariable String username, @RequestParam String password, @RequestBody List<String> trainerUsernames) {
+    @ApiOperation(value = "Update trainee's trainers list", response = ResponseEntity.class)
+    public ResponseEntity<List<TrainerListItem>> updateTraineeTrainers(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Password", required = true) @RequestParam String password,
+            @ApiParam(value = "List of trainer usernames", required = true) @RequestBody List<String> trainerUsernames) {
         List<TrainerListItem> updatedTrainersList = traineeService.updateTrainersList(username, password, trainerUsernames);
         return new ResponseEntity<>(updatedTrainersList, HttpStatus.OK);
     }
 
     // 12. Get Trainee Trainings List
     @GetMapping(value = "/trainees/{username}/trainings")
-    public ResponseEntity<List<TrainingResponse>> getTraineeTrainings(@PathVariable String username, @RequestBody TrainingFilteredDTO trainingFilteredDTO) {
+    @ApiOperation(value = "Get trainee's trainings list", response = ResponseEntity.class)
+    public ResponseEntity<List<TrainingResponse>> getTraineeTrainings(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Training filter", required = true) @RequestBody TrainingFilteredDTO trainingFilteredDTO) {
         trainingFilteredDTO.setUsername(username);
         return new ResponseEntity<>(trainingService.getTraineeTrainingsFiltered(trainingFilteredDTO), HttpStatus.OK);
     }
 
     // 13. Get Trainer Trainings List
     @GetMapping(value = "/trainers/{username}/trainings")
-    public ResponseEntity<List<TrainingResponse>> getTrainerTrainings(@PathVariable String username, @RequestBody TrainingFilteredDTO trainingFilteredDTO) {
+    @ApiOperation(value = "Get trainer's trainings list", response = ResponseEntity.class)
+    public ResponseEntity<List<TrainingResponse>> getTrainerTrainings(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Training filter", required = true) @RequestBody TrainingFilteredDTO trainingFilteredDTO) {
         trainingFilteredDTO.setUsername(username);
         return new ResponseEntity<>(trainingService.getTrainerTrainingsFiltered(trainingFilteredDTO), HttpStatus.OK);
     }
 
     // 14. Add Training
     @PostMapping(value = "/training")
-    public ResponseEntity<TrainingDTO> addTraining(@RequestBody TrainingDTO trainingDTO) {
+    @ApiOperation(value = "Add training", response = ResponseEntity.class)
+    public ResponseEntity<TrainingDTO> addTraining(
+            @ApiParam(value = "Training details", required = true) @RequestBody TrainingDTO trainingDTO) {
         return new ResponseEntity<>(trainingService.createTraining(trainingDTO), HttpStatus.CREATED);
     }
 
     // 15. Activate/Deactivate Trainee
     @PatchMapping(value = "/trainees/{username}/status")
-    public ResponseEntity<User> activateDeactivateTrainee(@PathVariable String username, @RequestParam String password, @RequestParam boolean isActive) {
+    @ApiOperation(value = "Activate or deactivate trainee", response = ResponseEntity.class)
+    public ResponseEntity<User> activateDeactivateTrainee(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Password", required = true) @RequestParam String password,
+            @ApiParam(value = "Active status", required = true) @RequestParam boolean isActive) {
         return new ResponseEntity<>(traineeService.ActivateDeactivateTrainee(username, password, isActive), HttpStatus.OK);
     }
 
     // 16. Activate/Deactivate Trainer
     @PatchMapping(value = "/trainers/{username}/status")
-    public ResponseEntity<User> activateDeactivateTrainer(@PathVariable String username, @RequestParam String password, @RequestParam boolean isActive) {
+    @ApiOperation(value = "Activate or deactivate trainer", response = ResponseEntity.class)
+    public ResponseEntity<User> activateDeactivateTrainer(
+            @ApiParam(value = "Username", required = true) @PathVariable String username,
+            @ApiParam(value = "Password", required = true) @RequestParam String password,
+            @ApiParam(value = "Active status", required = true) @RequestParam boolean isActive) {
         return new ResponseEntity<>(trainerService.ActivateDeactivateTrainer(username, password, isActive), HttpStatus.OK);
     }
 
     // 17. Get Training Types
     @GetMapping(value = "/training-Types")
+    @ApiOperation(value = "Get all training types", response = ResponseEntity.class)
     public ResponseEntity<List<TrainingType>> getAllTrainingTypes() {
         return new ResponseEntity<>(trainingTypeService.getAllTrainingTypes(), HttpStatus.OK);
     }
--- src/test/java/com/epam/taskgym/TraineeServiceTest.java
+++ src/test/java/com/epam/taskgym/TraineeServiceTest.java
@@ -2,9 +2,11 @@ package com.epam.taskgym;
 
 import com.epam.taskgym.dto.TraineeDTO;
 import com.epam.taskgym.entity.Trainee;
+import com.epam.taskgym.entity.Trainer;
 import com.epam.taskgym.entity.User;
 import com.epam.taskgym.exception.*;
 import com.epam.taskgym.repository.TraineeRepository;
+import com.epam.taskgym.repository.TrainerRepository;
 import com.epam.taskgym.repository.TrainingRepository;
 import com.epam.taskgym.service.TraineeService;
 import com.epam.taskgym.service.UserService;
@@ -15,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,6 +37,8 @@ class TraineeServiceTest {
     UserService userService;
     @Mock
     TrainingRepository trainingRepository;
+    @Mock
+    TrainerRepository trainerRepository;
 
     User user;
     Trainee trainee;
@@ -48,4 +54,101 @@ class TraineeServiceTest {
         trainee = new Trainee();
         trainee.setUser(user);
     }
+
+    @Test
+    void testGetTraineeByUsername() {
+        when(traineeRepository.findByUserUsername(user.getUsername())).thenReturn(Optional.of(trainee));
+
+        Trainee result = traineeService.getTraineeByUsername(user.getUsername());
+
+        assertEquals(trainee, result);
+    }
+
+    @Test
+    void testGetTraineeByUsername_NotFound() {
+        when(traineeRepository.findByUserUsername("unknown.username")).thenReturn(Optional.empty());
+
+        assertThrows(NotFoundException.class, () -> traineeService.getTraineeByUsername("unknown.username"));
+    }
+
+    @Test
+    void testRegisterTrainee() {
+        TraineeDTO traineeDTO = new TraineeDTO();
+        traineeDTO.setFirstName("John");
+        traineeDTO.setLastName("Doe");
+        traineeDTO.setDateOfBirth(new Date());
+
+        when(userService.createUser(traineeDTO.getFirstName(), traineeDTO.getLastName())).thenReturn(user);
+        when(traineeRepository.save(any(Trainee.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        Trainee newTrainee = traineeService.registerTrainee(traineeDTO);
+
+        assertEquals(traineeDTO.getFirstName(), newTrainee.getUser().getFirstName());
+        assertEquals(traineeDTO.getLastName(), newTrainee.getUser().getLastName());
+
+        verify(userService, times(1)).createUser(traineeDTO.getFirstName(), traineeDTO.getLastName());
+        verify(traineeRepository, times(1)).save(any(Trainee.class));
+    }
+
+    @Test
+    void testRegisterTrainee_NullTraineeDTO() {
+        assertThrows(MissingAttributes.class, () -> traineeService.registerTrainee(null));
+    }
+
+    @Test
+    void testUpdateTrainee_AuthenticationPasses() {
+        TraineeDTO traineeDTO = new TraineeDTO();
+        traineeDTO.setFirstName("John");
+        traineeDTO.setLastName("Doe");
+
+        when(userService.authenticateUser(any(String.class), any(String.class))).thenReturn(user);
+        when(traineeRepository.findByUserUsername(any(String.class))).thenReturn(Optional.of(trainee));
+        when(userService.updateUser(any(String.class), any(String.class), any(User.class))).thenReturn(user);
+        when(traineeRepository.save(any(Trainee.class))).thenAnswer(i -> i.getArguments()[0]);
+
+        Trainee updatedTrainee = traineeService.updateTrainee(traineeDTO, user.getUsername(), user.getPassword());
+
+        assertEquals("John", updatedTrainee.getUser().getFirstName());
+        assertEquals("Doe", updatedTrainee.getUser().getLastName());
+        verify(userService, times(1)).authenticateUser(any(String.class), any(String.class));
+        verify(userService, times(1)).updateUser(any(String.class), any(String.class), any(User.class));
+        verify(traineeRepository, times(2)).findByUserUsername(user.getUsername());
+        verify(traineeRepository, times(1)).save(any(Trainee.class));
+    }
+
+    @Test
+    void testUpdateTrainee_AuthenticationFails() {
+        TraineeDTO traineeDTO = new TraineeDTO();
+        traineeDTO.setFirstName("Updated");
+        traineeDTO.setLastName("User");
+
+        String wrongPassword = "wrongPassword";
+
+        when(userService.authenticateUser(any(String.class), any(String.class))).thenThrow(FailAuthenticateException.class);
+        assertThrows(FailAuthenticateException.class, () -> traineeService.updateTrainee(traineeDTO, user.getUsername(), wrongPassword));
+        verify(userService, times(1)).authenticateUser(any(String.class), any(String.class));
+        verify(userService, times(0)).updateUser(any(String.class), any(String.class), any(User.class));
+        verify(traineeRepository, times(0)).findByUserUsername(user.getUsername());
+        verify(traineeRepository, times(0)).save(any(Trainee.class));
+    }
+
+    @Test
+    void testDeleteTrainee() {
+        String username = "username";
+        String password = "password";
+
+        when(userService.authenticateUser(username, password)).thenReturn(user);
+        when(traineeRepository.findByUserUsername(username)).thenReturn(Optional.of(trainee));
+        when(trainingRepository.findAllTrainersByTraineeUsername(username)).thenReturn(new ArrayList<Trainer>());
+
+        assertDoesNotThrow(() -> traineeService.deleteTrainee(username, password));
+
+        verify(userService, times(1)).authenticateUser(username, password);
+        verify(traineeRepository, times(2)).findByUserUsername(username);
+        verify(trainingRepository, times(1)).findAllTrainersByTraineeUsername(username);
+        verify(trainingRepository, times(1)).deleteAllByTrainee_User_Username(username);
+        verify(traineeRepository, times(1)).delete(trainee);
+        verify(userService, times(1)).deleteUser(user);
+    }
+
 }
