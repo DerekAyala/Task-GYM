5d3e87a0ec4dc72530978d3494c921ccd4bcf5f4
Derek Ayala <derek_ayala@epam.com>
2024-02-28 10:23:34 -0600

    update User service with helpers

--- src/main/java/com/epam/taskgym/helpers/Validations.java
+++ src/main/java/com/epam/taskgym/helpers/Validations.java
@@ -10,8 +10,8 @@ public class Validations {
     public static void validateUserDetails(String firstName, String lastName) {
         LOGGER.info("Validating user details");
         if (firstName == null || firstName.isEmpty() || lastName == null || lastName.isEmpty()) {
-            LOGGER.error("First name and last name cannot be null or empty.");
-            throw new MissingAttributes("First name and last name cannot be null or empty.");
+            LOGGER.error("First name and last name are required.");
+            throw new MissingAttributes("First name and last name are required.");
         }
     }
 
