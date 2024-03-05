dd543f4e66cadd3ab95d8daa1af38b1d8a2ae51a
Derek Ayala <derek_ayala@epam.com>
2024-03-04 21:27:49 -0600

    add security filter chain

--- src/main/java/com/epam/taskgym/configuration/SecurityConfig.java
+++ src/main/java/com/epam/taskgym/configuration/SecurityConfig.java
@@ -2,12 +2,26 @@ package com.epam.taskgym.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("api/trainee", "api/trainer", "api/user/login").permitAll()
+                        .anyRequest().authenticated());
+        return http.build();
     }
 }
