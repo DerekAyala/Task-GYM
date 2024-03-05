1ebbb3d4b1f99ef40f9c2313fe945ad600e2af1a
Derek Ayala <derek_ayala@epam.com>
2024-03-05 10:27:32 -0600

    CORS policy in Spring Security

--- src/main/java/com/epam/taskgym/configuration/SecurityConfig.java
+++ src/main/java/com/epam/taskgym/configuration/SecurityConfig.java
@@ -4,8 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -19,9 +25,23 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
+                .cors().disable()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .formLogin().disable()
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("api/trainee", "api/trainer", "api/user/login").permitAll()
+                        .requestMatchers("api/trainee").permitAll()
+                        .requestMatchers("api/user/login").permitAll()
+                        .requestMatchers("api/trainer").permitAll()
                         .anyRequest().authenticated());
         return http.build();
+    }
+
+    CorsConfigurationSource apiConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
