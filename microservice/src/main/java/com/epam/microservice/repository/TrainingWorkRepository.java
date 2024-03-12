ff31467d3d47a770d14debb5f29e0bfb6526601a
Derek Ayala <derek_ayala@epam.com>
2024-03-12 17:13:48 -0600

    create all repositories

---
+++ microservice/src/main/java/com/epam/microservice/repository/TrainingWorkRepository.java
@@ -0,0 +1,9 @@
+package com.epam.microservice.repository;
+
+import com.epam.microservice.entity.TrainingWork;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrainingWorkRepository extends JpaRepository<TrainingWork, Long> {
+}
