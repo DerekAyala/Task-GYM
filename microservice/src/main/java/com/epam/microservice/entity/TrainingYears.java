6b4cb619e7bce6aa93124e39cc1987ee3ade7caa
Derek Ayala <derek_ayala@epam.com>
2024-03-13 16:21:21 -0600

    add to string annotation to check them

--- microservice/src/main/java/com/epam/microservice/entity/TrainingYears.java
+++ microservice/src/main/java/com/epam/microservice/entity/TrainingYears.java
@@ -3,12 +3,14 @@ package com.epam.microservice.entity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@ToString
 public class TrainingYears {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
