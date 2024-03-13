3650c4bfabea5cf6cde0da8ead41f5b1b3c05f96
Derek Ayala <derek_ayala@epam.com>
2024-03-12 19:00:17 -0600

    update global Exception Handler for my main app

--- Task-GYM/src/main/java/com/epam/taskgym/controller/GlobalExceptionHandler.java
+++ Task-GYM/src/main/java/com/epam/taskgym/controller/GlobalExceptionHandler.java
@@ -7,36 +7,36 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler{
 
     @ExceptionHandler(FailAuthenticateException.class)
-    public ResponseEntity<String> handleFailAuthenticateException(FailAuthenticateException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex.getMessage());
+    public ResponseEntity<Exception> handleFailAuthenticateException(FailAuthenticateException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex);
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<String> handleInvalidPasswordException(InvalidPasswordException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<Exception> handleInvalidPasswordException(InvalidPasswordException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
     }
 
     @ExceptionHandler(MissingAttributes.class)
-    public ResponseEntity<String> handleMissingAttributes(MissingAttributes ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    public ResponseEntity<Exception> handleMissingAttributes(MissingAttributes ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundException(NotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    public ResponseEntity<Exception> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex);
     }
 
     @ExceptionHandler(TraineeDeletionException.class)
-    public ResponseEntity<String> handleTraineeDeletionException(TraineeDeletionException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    public ResponseEntity<Exception> handleTraineeDeletionException(TraineeDeletionException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
     }
 
     // Handle generic exceptions
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
+    public ResponseEntity<Exception> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
     }
 }
