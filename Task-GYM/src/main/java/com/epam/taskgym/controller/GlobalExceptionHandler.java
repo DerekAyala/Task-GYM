0261228cdf8ecd05b19c0c28c445ae5e88570da4
Derek Ayala <derek_ayala@epam.com>
2024-03-19 17:31:40 -0600

    update exceptions on main app

--- Task-GYM/src/main/java/com/epam/taskgym/controller/GlobalExceptionHandler.java
+++ Task-GYM/src/main/java/com/epam/taskgym/controller/GlobalExceptionHandler.java
@@ -6,37 +6,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @ControllerAdvice
 public class GlobalExceptionHandler{
 
     @ExceptionHandler(FailAuthenticateException.class)
-    public ResponseEntity<Exception> handleFailAuthenticateException(FailAuthenticateException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ex);
+    public ResponseEntity<ErrorResponse> handleFailAuthenticateException(FailAuthenticateException ex) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ErrorResponse(ex.getClass().getName(), ex.getMessage(), LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(), ex.getCause(), ex.getLocalizedMessage(), List.of(ex.getStackTrace())));
     }
 
     @ExceptionHandler(InvalidPasswordException.class)
-    public ResponseEntity<Exception> handleInvalidPasswordException(InvalidPasswordException ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
+    public ResponseEntity<ErrorResponse> handleInvalidPasswordException(InvalidPasswordException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getClass().getName(), ex.getMessage(), LocalDateTime.now(), HttpStatus.UNAUTHORIZED.value(), ex.getCause(), ex.getLocalizedMessage(), List.of(ex.getStackTrace())));
     }
 
     @ExceptionHandler(MissingAttributes.class)
-    public ResponseEntity<Exception> handleMissingAttributes(MissingAttributes ex) {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
+    public ResponseEntity<ErrorResponse> handleMissingAttributes(MissingAttributes ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(ex.getClass().getName(), ex.getMessage(), LocalDateTime.now(), HttpStatus.BAD_REQUEST.value(), ex.getCause(), ex.getLocalizedMessage(), List.of(ex.getStackTrace())));
     }
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<Exception> handleNotFoundException(NotFoundException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex);
+    public ResponseEntity<ErrorResponse> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(ex.getClass().getName(), ex.getMessage(), LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getCause(), ex.getLocalizedMessage(), List.of(ex.getStackTrace())));
     }
 
     @ExceptionHandler(TraineeDeletionException.class)
-    public ResponseEntity<Exception> handleTraineeDeletionException(TraineeDeletionException ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
+    public ResponseEntity<ErrorResponse> handleTraineeDeletionException(TraineeDeletionException ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getClass().getName(), ex.getMessage(), LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getCause(), ex.getLocalizedMessage(), List.of(ex.getStackTrace())));
     }
 
     // Handle generic exceptions
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Exception> handleException(Exception ex) {
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex);
+    public ResponseEntity<ErrorResponse> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ErrorResponse(ex.getClass().getName(), ex.getMessage(), LocalDateTime.now(), HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getCause(), ex.getLocalizedMessage(), List.of(ex.getStackTrace())));
     }
 }
