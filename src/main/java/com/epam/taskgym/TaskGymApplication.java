1d7dbc1e8478f662a75419402daac41c5dbe2953
Derek Ayala <derek_ayala@epam.com>
2024-03-19 19:41:32 -0600

    prepare connection by application properties

--- src/main/java/com/epam/taskgym/TaskGymApplication.java
+++ src/main/java/com/epam/taskgym/TaskGymApplication.java
@@ -2,8 +2,10 @@ package com.epam.taskgym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class TaskGymApplication {
 
 	public static void main(String[] args) {
