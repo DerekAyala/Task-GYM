ef40b794f4e23a9f274602ea83c9d8cc7d251ba1
Derek Ayala <derek_ayala@epam.com>
2024-03-20 11:14:18 -0600

    add no and all args constructor

--- src/main/java/com/epam/taskgym/models/TrainingRequest.java
+++ src/main/java/com/epam/taskgym/models/TrainingRequest.java
@@ -1,14 +1,13 @@
 package com.epam.taskgym.models;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
 @Setter
-@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class TrainingRequest {
     private String username;
     private String firstName;
